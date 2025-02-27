208465da5ed44b8d5f03663a17342a3391c3576a
Ruslan <yaremkoruslan69@gmail.com>
2025-02-27 14:11:03 +0200

    1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'wbody.webart.work',
   appName: 'wBody',
-  webDir: 'dist/app/browser'
+  webDir: 'dist/app/browser',
+  plugins: {
+    AdMob: {
+      androidAdMobAppId: 'ca-app-pub-3940256099942544~3347511713', // Test App ID
+    }
+  }
 };
 
 export default config;
